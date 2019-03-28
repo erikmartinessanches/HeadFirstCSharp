cd6a6c39c59832956b46ffc8e1cd213baa067f5e
Erik Martines Sanches <erik.martinessanches@gmail.com>
2019-03-28 12:57:52 +0100

    Animating Winforms apps.

---
+++ Chapter2Example3/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Chapter2Example3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.somethingElse = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(225, 174);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // somethingElse
+            // 
+            this.somethingElse.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.somethingElse.Location = new System.Drawing.Point(0, 0);
+            this.somethingElse.Name = "somethingElse";
+            this.somethingElse.Size = new System.Drawing.Size(125, 50);
+            this.somethingElse.TabIndex = 1;
+            this.somethingElse.Text = "Did this change in the IDE?";
+            this.somethingElse.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.somethingElse);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button somethingElse;
+    }
+}
+
---
+++ Chapter2_AnimatingWinFormsApps/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter2_AnimatingWinFormsApps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (Visible) //Visible is true as long as the form is displayed.
+            {
+                //Thee test for Visible in the for loop allows us to exit the 
+                //loop as soon as the application is closed with "x"-button.
+                for (int c = 0; c < 254 && Visible; c++)
+                {
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+
+                    /* Stop looping momentarily and do the other things it 
+                     * needs to like refreshing the form, check for input, etc.
+                     * If we omit this line, the form doesn't redraw itself, 
+                     * because it waits for the loop to finish. This is kind 
+                     * of a hack, to be used only for toy programs. */
+                    Application.DoEvents();
+
+                    System.Threading.Thread.Sleep(3); //In milliseconds.
+                }
+
+                for (int c = 254; c >= 0 && Visible; c--)
+                {
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(3);
+                }
+            }
+
+        }
+    }
+}
