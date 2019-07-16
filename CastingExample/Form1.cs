ce3e4a0d537c215365745e7cd4694c8adeaf7fc4
Erik Martines Sanches <erik.martinessanches@gmail.com>
2019-07-16 03:36:52 +0200

    Casting example and Milage calculator from chapter 4.

---
+++ CastingExample/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CastingExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int myOriginalValue = 259;
+            byte myConvertedValue = (byte)myOriginalValue;
+            MessageBox.Show("The converted (cast) value is " + myConvertedValue
+                + ".");
+
+            /* If I use +, *, / or - with two different types, it automatically
+             * converts the smaller type into the bigger one: */
+            int myInt = 36;
+            float myFloat = 16.4F;
+            myFloat = myInt + myFloat;
+            MessageBox.Show("myFloat is " + myFloat + ".");
+        }
+
+        /* Examples of various casts. */
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int myInt = 10;
+            byte myByte = (byte)myInt;
+            double myDouble = (double)myByte;
+            //bool myBool = (bool)myDouble; //Doesn't work
+            string myString = "false";
+            //myBool = (bool)myString; //Doesn't work
+            //myString = (string)myInt; //Doesn't work
+            myString = myInt.ToString();
+            //myBool = (bool)myByte; //Doesn't work
+            //myByte = (byte)myBool;    //Doesn't work
+            short myShort = (short)myInt;
+            char myChar = 'x';
+            //myString = (string)myChar; //Doesn't work
+            long myLong = (long)myInt;
+            decimal myDecimal = (decimal)myLong;
+            myString = myString + myInt + myByte + myDouble + myChar;
+
+
+        }
+    }
+}
---
+++ Reimbursement calculator/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reimbursement_calculator
+{
+    public partial class Form1 : Form
+    {
+        int startingMileage;
+        int endingMileage;
+        double milesTraveled;
+        double reimburseRate;
+        double amountOwed;
+
+        public Form1()
+        {
+            reimburseRate = 0.39;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startingMileage = (int)numericUpDown1.Value;
+            endingMileage = (int)numericUpDown2.Value;
+            if (startingMileage < endingMileage)
+            {
+                milesTraveled = endingMileage - startingMileage;
+                amountOwed = milesTraveled * reimburseRate;
+                label4.Text = "SEK " + amountOwed;
+            } else
+            {
+                MessageBox.Show("The starting mileage must be less than the " +
+                    "ending mileage.", "Cannot calculate mileage.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(milesTraveled + " miles.", "Miles traveled");
+        }
+    }
+}
