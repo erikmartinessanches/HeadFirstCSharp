81aeafc7ca0d6054098182862ee3146a7847fb02
Erik Martines Sanches <erik.martinessanches@gmail.com>
2021-02-13 16:23:25 +0100

    Out modifier example.

---
+++ Chapter11/OutModifier/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OutModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number. ");
+            //"Out" and "ref" parameters are passed by reference.
+            //TryParse uses the "out" modifier to convert a string to an int.
+            if (int.TryParse(Console.ReadLine(), out int input))
+            {
+                //We also need to use the "out" modifier when calling a method that takes "out" parameters.
+                var output1 = ReturnThreeValues(input, out double output2, out int output3);
+                Console.WriteLine($"Outputs: plus one = {output1}, half = {output2:F}, twice={output3}");
+            }
+        }
+
+        //Returning more than one value by using "out" parameters.
+        public static int ReturnThreeValues(int value, out double half, out int twice)
+        {
+            //All "out" parameters must be assigned before the method exits.
+            half = value / 2f;
+            twice = value * 2;
+            return value + 1;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # HeadFirstCSharp
-Since I have a programming background (mainly with Java nad some C++), I've done select examples from chapter 2-7 in "Head First C#" book (3rd edition) and chapters 8-11 from edition four in order to learn C#.
+Since I have a programming background (mainly with Java nad some C++), I've done select examples from chapter 2-7 in "Head First C#" book (3rd edition) and chapters 8-12 from edition four in order get up to speed with C#.
 
 ![something](https://covers.oreillystatic.com/images/0636920027812/cat.gif "something")
