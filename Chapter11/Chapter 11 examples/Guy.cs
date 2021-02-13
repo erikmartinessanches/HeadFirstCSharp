10b6ef374aaafc163e5a939eaed78669f04d87da
Erik Martines Sanches <erik.martinessanches@gmail.com>
2021-02-13 16:37:38 +0100

    Ref parameters example.

---
+++ Chapter11/Chapter 11 examples/Guy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter_11_examples
+{
+    class Guy
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public override string ToString() => $"a {Age}-year-old named {Name}";
+    }
+}
---
+++ Chapter11/Chapter 11 examples/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Chapter_11_examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 1;
+            var guy = new Guy() { Name = "Joe", Age = 26 };
+            Console.WriteLine("i is {0} and guy is {1}", i, guy);
+            ModifyAnIntAndGuy(ref i, ref guy);
+            Console.WriteLine("Now i is {0} and guy is {1}", i, guy);
+        }
+
+        /* A "ref" parameter is like an "out" parameter, except that it doesn't 
+        * need to be assigned prior to calling the function with the ref and it must
+        * be assigned before the method returns.
+        */
+        static void ModifyAnIntAndGuy(ref int valueRef, ref Guy guyRef)
+        {
+            valueRef += 10;
+            guyRef.Name = "Bob";
+            guyRef.Age = 37;
+        }
+        /* Remember that value types have a TryParse method that uses "out" parameters.
+         * For example: double.TryParse() and Enum.TryParse().
+         */
+    }
+}
