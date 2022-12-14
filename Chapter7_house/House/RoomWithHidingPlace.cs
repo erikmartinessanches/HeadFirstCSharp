40eb12caeaf7dad8bc27d14924459bd26ff0542a
Erik Martines Sanches <erik.martinessanches@gmail.com>
2022-12-14 02:40:37 +0100

    Resolving a build error.

--- Chapter7_house/House/RoomWithHidingPlace.cs
+++ Chapter7_house/House/RoomWithHidingPlace.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting;
 using System.Text;
 using System.Threading.Tasks;
 
