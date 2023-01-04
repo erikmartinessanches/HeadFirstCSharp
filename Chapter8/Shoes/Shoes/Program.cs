4a5195b9629edcab7ccd77321c0fa5060644d6c7
Erik Martines Sanches <erik.martinessanches@gmail.com>
2023-01-04 22:03:21 +0100

    Completing the  Shoes problem in Chapter 8.

--- Chapter8/Shoes/Shoes/Program.cs
+++ Chapter8/Shoes/Shoes/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Shoes
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+﻿namespace Shoes;
+
+internal class Program {
+    private static ShoeCloset shoeCloset = new();
+
+    private static void Main(string[] args) {
+        while (true) {
+            shoeCloset.PrintShoes();
+            Console.Write("Press 'a' to add or 'r' to remove a shoe. ");
+            var key = Console.ReadKey().KeyChar;
+            switch (key) {
+                case 'a':
+                case 'A':
+                    shoeCloset.AddShoe();
+                    break;
+                case 'r':
+                case 'R':
+                    shoeCloset.RemoveShoe();
+                    break;
+                default: return;
+            }
         }
     }
 }
--- Chapter8/Shoes/Shoes/ShoeCloset.cs
+++ Chapter8/Shoes/Shoes/ShoeCloset.cs
@@ -1,39 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Shoes;
 
-namespace Shoes
-{
-    internal class ShoeCloset
-    {
-        private readonly List<Shoe> shoes = new List<Shoe>();
+internal class ShoeCloset {
+    private readonly List<Shoe> shoes = new();
 
-        public void PrintShoes()
-        {
-            if (shoes.Count == 0)
-            {
-                Console.WriteLine("The closet is empty.");
-            }
-            else
-            {
-                Console.WriteLine("The closet contains: ");
-                int i = 1;
-                foreach (Shoe shoe in shoes)
-                {
-                    Console.WriteLine($"Shoe number {i}: {shoe.Description}.");
-                }
-            }
+    public void PrintShoes() {
+        if (shoes.Count == 0) {
+            Console.WriteLine("The closet is empty.");
         }
-        public void AddShoe()
-        {
-            Console.WriteLine("Add a shoe to the closet.");
-            for (int i = 0; i < 6; i++)
-            {
-                //In {(Style)i} the Style enum's ToSting() is implicitly called by the interpolated string.
-                Console.WriteLine($"Press {i} to add style {(Style)i}");
-            }
+        else {
+            Console.WriteLine("The closet contains: ");
+            var i = 1;
+            foreach (var shoe in shoes)
+                Console.WriteLine($"Shoe number {i++}: {shoe.Description}.");
+        }
+    }
+
+    public void AddShoe() {
+        Console.WriteLine("\nAdd a shoe to the closet.");
+        for (var i = 0; i < 6; i++)
+            //In {(Style)i} the Style enum's ToSting() is implicitly called by the interpolated string.
+            Console.WriteLine($"Press {i} to add style {(Style)i}");
+        Console.Write("Enter a shoe style: ");
+        if (int.TryParse(Console.ReadKey().KeyChar.ToString(), out var style)) {
+            Console.Write("\nEnter the shoe color: ");
+            var color = Console.ReadLine();
+            Shoe shoe = new((Style)style, color);
+            shoes.Add(shoe);
+        }
+    }
+
+    public void RemoveShoe() {
+        Console.Write("\nEnter shoe numer to remove: ");
+        if (int.TryParse(Console.ReadKey().KeyChar.ToString(),
+                out var shoeNumberToRemove) && shoeNumberToRemove >= 1 &&
+            shoeNumberToRemove <= shoes.Count) {
+            Console.WriteLine(
+                $"Removing {shoes[shoeNumberToRemove - 1].Description}");
+            shoes.RemoveAt(shoeNumberToRemove - 1);
         }
     }
 }
