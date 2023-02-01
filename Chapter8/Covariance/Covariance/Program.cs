62f684463a421db05f7f264a357ac942fc5b5055
Erik Martines Sanches <erik.martinessanches@gmail.com>
2023-02-01 18:11:23 +0100

    Some more commenting. This should conclude chapter 8.

--- Chapter8/Covariance/Covariance/Program.cs
+++ Chapter8/Covariance/Covariance/Program.cs
@@ -1,7 +1,12 @@
 ﻿/*Covariance is when we take a collection, such as a List<T>, and upcast the
  entire collection at once. Covariance is implicitly converting a subclass
  reference to its superclass. Useful when we need o pass a collection to a method
- that only workd with the superclass type.*/
+ that only workd with the superclass type.
+
+ Generic .NET collections implement IEnumerable. (So we can always look over 
+ them.) Also, it is possible to create a List, Queue or Stack from any other 
+ object that implements the IEnumerable<T> interface (by passing to the overloaded
+constructor a collection we want to *copy* from). */
 
 using System;
 using System.Collections.Generic;
@@ -19,13 +24,14 @@ namespace Covariance {
         new Duck { Kind = KindOfDuck.Mallard, Size = 14 },
         new Duck { Kind = KindOfDuck.Loon, Size = 13 }
       };
-      /*We *can* use assignment = to upcast List<Duck> to an IEnumerable<Bird>.*/
+      /*Covariance allows us to simply use assignment = to upcast List<Subclass>
+        to an IEnumerable<Superclass>.*/
       IEnumerable<Bird> upcastDucks = ducks;
       /*Now, using IEnumerable's ToList() converts the IEnumerable<Bird> to
-       List<Bird>.*/
+        List<Bird>.*/
       Bird.FlyAway(upcastDucks.ToList(), "Minnesota");
-      /* Bird.FlyAway() is a static method on the Bird class, hence it's
-      availability here. */
+      /*Bird.FlyAway() is a static method on the Bird class, hence it's
+        availability here. */
     }
   }
 }
