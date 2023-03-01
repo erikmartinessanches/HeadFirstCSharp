f7b69ed22a265fa70ac66f51021172e9d24e7f6b
Erik Martines Sanches <erik.martinessanches@gmail.com>
2023-03-01 17:33:43 +0100

    JsonSerializer example, chapter 10.

---
+++ Chapter 10/JSONSerialization/JSONSerialization/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+
+namespace JSONSerialization {
+   internal class Program {
+      static void Main(string[] args) {
+         var guys = new List<Guy>() { new Guy() { Name="Juno", Clothes = new Outfit() { Top="Space t-shirt", Bottom="Space pants" }, Hair=new HairStyle() { Color=HairColor.Blue, Length=3.5f } },
+            new Guy() { Name="Lupus", Clothes = new Outfit() { Top="Collar", Bottom="Tank" }, Hair=new HairStyle() { Color=HairColor.White, Length=1.5f } }
+            };
+
+         //Now serializing guys.
+         var jsonString = JsonSerializer.Serialize(guys);
+         /*Enums are ancoded as integer values in jsonString. */
+         Console.WriteLine(jsonString);
+
+         /*Deserializing jsonString into new objects on the heap.*/
+         var copyOfGuys = JsonSerializer.Deserialize<List<Guy>>(jsonString);
+         foreach (var guy in copyOfGuys) {
+            Console.WriteLine($"This guy was deserialized: {guy}");
+         }
+      }
+
+      class Guy {
+         public string Name { get; set; }
+         public HairStyle Hair { get; set; }
+         public Outfit Clothes { get; set; }
+         public override string ToString() => $"{Name} with {Hair} wearing {Clothes}";
+      }
+      class Outfit {
+         public string Top { get; set; }
+         public string Bottom { get; set; }
+         public override string ToString() => $"{Top} and {Bottom}";
+      }
+      enum HairColor {
+         Auburn, Black, Blonde, Blue, Brown, Gray, Platinum, Purple, Red, White
+      }
+      class HairStyle {
+         public HairColor Color { get; set; }
+         public float Length { get; set; }
+         public override string ToString() => $"{Length:0.0} inch {Color} hair";
+      }
+   }
+}
