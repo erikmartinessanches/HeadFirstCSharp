ae885fb5f2c2bdbbc435b677cbe02a4839890968
Erik Martines Sanches <erik.martinessanches@gmail.com>
2023-06-15 23:23:00 +0200

    Updating the targeted dotnet sdk in one project.

--- Chapter11/Chapter 11 examples/Guy.cs
+++ Chapter11/Chapter 11 examples/Guy.cs
@@ -1,25 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-//Treat any reference as a non-nullable reference type.
+﻿//Treat any reference as a non-nullable reference type.
 #nullable enable
 
-namespace Chapter_11_examples
-{
-    class Guy
-    {
-        /*Usually we would not use public fields. */
-        public string? Name { get; set; } //Name is now a nullable reference type...
-        public int Age { get; set; }
-        public override string ToString() => $"a {Age}-year-old named {Name}";
+namespace Chapter_11_examples {
+   class Guy {
+      /*Usually we would not use public fields. */
+      public string? Name { get; set; } //Name is now a nullable reference type...
+      public int Age { get; set; }
+      public override string ToString() => $"a {Age}-year-old named {Name}";
 
-        public Guy(int age, string name)
-        {
-            Age = age;
-            Name = name; //Preventing Name from being set to null.
-        }
-        
-    }
+      public Guy(int age, string name) {
+         Age = age;
+         Name = name; //Preventing Name from being set to null.
+      }
+
+   }
 
 }
