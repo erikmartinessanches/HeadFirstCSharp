f6df2151395ae19f8b72dd050532331764f7466d
Erik Martines Sanches <erik.martinessanches@gmail.com>
2023-07-09 05:42:29 +0200

    Func and action delegates with LINQ.

---
+++ EventsAndDelegatesBonusChapter/ExploreFuncAndAction/ExploreFuncAndAction/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ExploreFuncAndAction {
+   internal class Program {
+      static void Main(string[] args) {
+         Func<int, string> timesFour = (int i) => $"-> {i * 4} <-";
+
+         int lineNumber = 1;
+         Action<string> writeLine = (string s) => Console.WriteLine($"Line {lineNumber} is {s}");
+
+         Enumerable.Range(1, 5).Select(timesFour).ToList().ForEach(writeLine);
+      }
+   }
+}
